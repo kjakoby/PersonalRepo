d0f521f444e0fc7b30932afeab0a254108535ba0
Kyle Jakoby <k.jakoby93@gmail.com>
2018-07-23 21:11:52 -0400

    Updated CreatureArena 7-23-18 9:11pm

---
+++ CreatureArena/CreatureArena/CreatureArena.UI/Controllers/ArenaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreatureArena.UI.Controllers
+{
+    public class ArenaController : Controller
+    {
+        // GET: Arena
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CreatureArena/CreatureArena/CreatureArena.UI/Controllers/DirectoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreatureArena.UI.Controllers
+{
+    public class DirectoryController : Controller
+    {
+        // GET: Directory
+        public ActionResult AllCreatures()
+        {
+            return View();
+        }
+
+        public ActionResult Creature(int id)
+        {
+            return View();
+        }
+    }
+}
