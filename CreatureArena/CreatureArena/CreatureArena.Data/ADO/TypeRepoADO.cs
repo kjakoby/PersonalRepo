88f8fed460b8c4e81bdd4e7e8adbb3974a4824b2
Kyle Jakoby <k.jakoby93@gmail.com>
2018-08-08 17:20:12 -0400

    Updated CreatureArena 8-8-18 5:20pm

--- CreatureArena/CreatureArena/CreatureArena.Data/ADO/TypeRepoADO.cs
+++ CreatureArena/CreatureArena/CreatureArena.Data/ADO/TypeRepoADO.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CreatureArena.Models.Tables;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace CreatureArena.Data.ADO
 {
@@ -12,12 +14,46 @@ namespace CreatureArena.Data.ADO
     {
         public List<Models.Tables.Type> GetAll()
         {
-            throw new NotImplementedException();
+            List<Models.Tables.Type> types = new List<Models.Tables.Type>();
+
+            using (var cn = new SqlConnection(Settings.GetConnectionString()))
+            {
+                SqlCommand cmd = new SqlCommand("TypeSelectAll", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Models.Tables.Type currentRow = new Models.Tables.Type();
+                        currentRow.TypeID = (int)dr["TypeID"];
+                        currentRow.TypeName = dr["TypeName"].ToString();
+                        currentRow.TypeIcon = dr["TypeIcon"].ToString();
+                        currentRow.TypeStrength = (int)dr["TypeStrength"];
+                        currentRow.TypeWeakness = (int)dr["TypeWeakness"];
+
+                        types.Add(currentRow);
+                    }
+                }
+            }
+
+            return types;
         }
 
         public Models.Tables.Type GetByID(int typeID)
         {
+            //need to complete
+
             throw new NotImplementedException();
+
+            //Models.Tables.Type type = null;
+
+            //using (var cn = new SqlConnection(Settings.GetConnectionString()))
+            //{
+            //    SqlCommand cmd = new SqlCommand()
+            //}
         }
     }
 }
